583d1cd7c25cae6dbc2e813a843e309fc79c4754
MathManiacc <73884571+MathManiacc@users.noreply.github.com>
2020-12-15 18:26:06 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,21 @@
+class Particle{
+    constructor(x, y){
+        var options = {
+            restitution: 0.5
+        }
+        this.body = Bodies.circle(x, y, 15, options);
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(this.color);
+        noStroke();
+        ellipse(0, 0, 15, 15);
+        pop();
+    }
+}
